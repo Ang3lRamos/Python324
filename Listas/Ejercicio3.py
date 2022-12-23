060d59c0ffe92c6c9933f445f4f7370c75d2ec06
Ang3lRamos <angelf.ramosc@unac.edu.co>
2022-12-22 23:41:58 -0500

    Correcion corta

--- Listas/Ejercicio3.py
+++ Listas/Ejercicio3.py
@@ -1,21 +1,21 @@
 #Almacene en una lista 6 números y obtenga la cantidad de pares e impares.
 
-    lista = []
+lista = []
 
-    cont = 0
+cont = 0
 
-    par = 0
-    impar = 0
+par = 0
+impar = 0
 
-    while cont < 6:
-        dig_num = int(input("Digite los numeros lista: "))
-        lista.append(dig_num)
-        cont += 1
+while cont < 6:
+    dig_num = int(input("Digite los numeros lista: "))
+    lista.append(dig_num)
+    cont += 1
 
-    for i in lista:
-        if i % 2 == 0:
-            par+=1
-        else:
-            impar +=1
+for i in lista:
+    if i % 2 == 0:
+        par+=1
+    else:
+        impar +=1
 
-    print(f"La lista tiene {par} pares y {impar} impares.")
+print(f"La lista tiene {par} pares y {impar} impares.")
